8082fe07bef46689b1506de8c82f0a1645fde64c
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2024-12-17 01:02:53 +0530

    gsap methods

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,5 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(()=>{
+    gsap.to("#text",{
+        y:0,
+        ease:"power1.inOut",
+        opacity:1,
+    })
+
+    gsap.fromTo(".para",{
+      y:20,
+      opacity:0,
+    },
+  {
+    y:0,
+    ease:"power1.inOut",
+    opacity:1,
+    delay:0.3,
+    stagger:0.1
+  })
+    
+
+  },[])
 
   return (
     <main>
